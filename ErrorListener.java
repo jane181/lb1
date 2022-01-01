c108e89e8fb0c399ebb46a418131ed03faad1b78
jane181 <39273502+jane181@users.noreply.github.com>
2022-01-01 11:55:59 +0800

    Update ErrorListener.java

--- ErrorListener.java
+++ ErrorListener.java
@@ -1,37 +1,34 @@
-
-import org.antlr.v4.runtime.ANTLRErrorListener;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import java.util.BitSet;
 
-public class ErrorListener implements ANTLRErrorListener {
+public class ErrorListener extends BaseErrorListener  {
 
     @Override
-    public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
-        System.exit(-1);
-
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+            throws ParseCancellationException {
+        System.exit(1);
     }
 
-    @Override
-    public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
-        System.exit(-1);
+ //   @Override
+//     public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
+//         System.exit(-1);
 
-    }
-
-
-    @Override
-    public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
-        System.exit(-1);
-
-    }
+//     }
 
 
-    @Override
-    public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
-        System.exit(-1);
-    }
-}
+//     @Override
+//     public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
+//         System.exit(-1);
+
+//     }
+
+
+//     @Override
+//     public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
+//         System.exit(-1);
+//     }
+ }
