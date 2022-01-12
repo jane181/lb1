a2de76f0024dfabd900e1d42b4ef40254394bb1b
jane181 <39273502+jane181@users.noreply.github.com>
2022-01-12 23:35:08 +0800

    Update main.java

--- main.java
+++ main.java
@@ -10,82 +10,34 @@ public class main {
     public static String outputPath = "";
 
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream(args[0]);
-        outputPath = args[1];
+    public static void main(String[] args) throws IOException {
+            ErrorListener e = new ErrorListener();
+            FileInputStream file = new FileInputStream(args[0]);
+            String outputPath = "";
+            outputPath = args[1];
 
-        byte[] arr = new byte[100];
-        String s = "";
-        int i = 0;
+            byte[] arr = new byte[100];
+            String str = "";
+            int i = 0;
 
-        while ((i = fis.read(arr, 0, 100)) != -1) {
-            s += new String(arr, 0, i);
-        }
-
-        String str = "";
-        boolean flag = false;
-        boolean flag2 = false;
-        while (true) {
-            if (s.charAt(0) == '/' && s.charAt(1) == '/' && !flag2) {
-                flag = true;
-                if (s.length() == 2) {
-                    break;
-                }
-                s = s.substring(2);
-                continue;
+            while ((i = file.read(arr, 0, 100)) != -1) {
+                str += new String(arr, 0, i);
             }
-            if (s.charAt(0) == '/' && s.charAt(1) == '*' && !flag) {
-                flag2 = true;
-                if (s.length() == 2) {
-                    break;
-                }
-                s = s.substring(2);
-                continue;
-            }
-            if (s.charAt(0) == '\n' && flag) {
-                flag = false;
-                str += "\n";
-                if (s.length() == 1) {
-                    break;
-                }
-                s = s.substring(1);
-                continue;
-            }
-            if (s.charAt(0) == '*' && s.charAt(1) == '/' && flag2) {
-                flag2 = false;
-                if (s.length() == 2) {
-                    break;
-                }
-                s = s.substring(2);
-                continue;
-            }
-            if (!flag && !flag2) {
-                str += s.charAt(0);
-                if (s.length() == 1) {
-                    break;
-                }
-                s = s.substring(1);
-                continue;
-            }
-            if (s.length() == 1) {
-                break;
-            }
-            s = s.substring(1);
-        }
-        if (flag2) {
-            System.out.println("error");
-            System.exit(1);
-        }
-
-
+       PrintStream ps = null;
+//        File file = new File(args[1]);
+//
+//        try {
+        ps = new PrintStream(new FileOutputStream(outputPath));
+    
         CharStream inputStream = CharStreams.fromString(str); // 获取输入流
         lab1Lexer lexer = new lab1Lexer(inputStream);
         lexer.removeErrorListeners();
-        lexer.addErrorListener(ErrorListener.INSTANCE);
+        lexer.addErrorListener(e);
 
         CommonTokenStream tokenStream = new CommonTokenStream(lexer); // 词法分析获取 token 流
         lab1Parser parser = new lab1Parser(tokenStream);
         parser.removeErrorListeners();
-        parser.addErrorListener(ErrorListener.INSTANCE);
+        parser.addErrorListener(e);
 
         ParseTree tree = parser.compUnit(); // 获取语法树的根节点
         // System.out.println(tree.toStringTree(parser)); // 打印字符串形式的语法树
@@ -93,6 +45,6 @@ public class main {
         Visitor visitor = new Visitor();
         visitor.visit(tree);
 
-        fis.close();
+        file.close();
     }
 }
