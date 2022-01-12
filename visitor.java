48b68edbf4251735d2842a07b68dc059ef73fe52
jane181 <39273502+jane181@users.noreply.github.com>
2022-01-12 23:06:38 +0800

    Update visitor.java

--- visitor.java
+++ visitor.java
@@ -2,8 +2,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
-public class Visitor extends lab2BaseVisitor<Void> {
-    public PrintStream ps = new PrintStream(new FileOutputStream(Test.outputPath));
+public class Visitor extends lab1BaseVisitor<Void> {
+    public PrintStream ps = new PrintStream(new FileOutputStream(main.outputPath));
     public static String exp = "";
 
     public Visitor() throws FileNotFoundException {
@@ -11,30 +11,30 @@ public class Visitor extends lab2BaseVisitor<Void> {
     }
 
     @Override
-    public Void visitCompUnit(lab2Parser.CompUnitContext ctx) {
+    public Void visitCompUnit(lab1Parser.CompUnitContext ctx) {
         return super.visitCompUnit(ctx);
     }
 
     @Override
-    public Void visitFuncDef(lab2Parser.FuncDefContext ctx) {
+    public Void visitFuncDef(lab1Parser.FuncDefContext ctx) {
         System.out.print("define dso_local ");
         return super.visitFuncDef(ctx);
     }
 
     @Override
-    public Void visitFuncType(lab2Parser.FuncTypeContext ctx) {
+    public Void visitFuncType(lab1Parser.FuncTypeContext ctx) {
         System.out.print("i32 ");
         return null;
     }
 
     @Override
-    public Void visitIdent(lab2Parser.IdentContext ctx) {
+    public Void visitIdent(lab1Parser.IdentContext ctx) {
         System.out.print("@main() ");
         return null;
     }
 
     @Override
-    public Void visitBlock(lab2Parser.BlockContext ctx) {
+    public Void visitBlock(lab1Parser.BlockContext ctx) {
         System.out.println("{");
         visit(ctx.stmt());
         System.out.println("}");
@@ -42,7 +42,7 @@ public class Visitor extends lab2BaseVisitor<Void> {
     }
 
     @Override
-    public Void visitStmt(lab2Parser.StmtContext ctx) {
+    public Void visitStmt(lab1Parser.StmtContext ctx) {
         visit(ctx.exp());
         System.out.print("    ret i32 ");
       //  new PostfixExpression().func(exp);
@@ -50,39 +50,39 @@ public class Visitor extends lab2BaseVisitor<Void> {
     }
 
     @Override
-    public Void visitExp(lab2Parser.ExpContext ctx) {
+    public Void visitExp(lab1Parser.ExpContext ctx) {
         return super.visitExp(ctx);
     }
 
     @Override
-    public Void visitAddExp(lab2Parser.AddExpContext ctx) {
+    public Void visitAddExp(lab1Parser.AddExpContext ctx) {
         return super.visitAddExp(ctx);
     }
 
     @Override
-    public Void visitAddSub(lab2Parser.AddSubContext ctx) {
+    public Void visitAddSub(lab1Parser.AddSubContext ctx) {
         exp += ctx.getText();
         return super.visitAddSub(ctx);
     }
 
     @Override
-    public Void visitMulExp(lab2Parser.MulExpContext ctx) {
+    public Void visitMulExp(lab1Parser.MulExpContext ctx) {
         return super.visitMulExp(ctx);
     }
 
     @Override
-    public Void visitMulDiv(lab2Parser.MulDivContext ctx) {
+    public Void visitMulDiv(lab1Parser.MulDivContext ctx) {
         exp += ctx.getText();
         return super.visitMulDiv(ctx);
     }
 
     @Override
-    public Void visitUnaryExp(lab2Parser.UnaryExpContext ctx) {
+    public Void visitUnaryExp(lab1Parser.UnaryExpContext ctx) {
         return super.visitUnaryExp(ctx);
     }
 
     @Override
-    public Void visitPrimaryExp(lab2Parser.PrimaryExpContext ctx) {
+    public Void visitPrimaryExp(lab1Parser.PrimaryExpContext ctx) {
         if (ctx.number() != null) {
             visit(ctx.number());
         } else {
@@ -94,13 +94,13 @@ public class Visitor extends lab2BaseVisitor<Void> {
     }
 
     @Override
-    public Void visitUnaryOp(lab2Parser.UnaryOpContext ctx) {
+    public Void visitUnaryOp(lab1Parser.UnaryOpContext ctx) {
         exp += ctx.getText();
         return super.visitUnaryOp(ctx);
     }
 
     @Override
-    public Void visitNumber(lab2Parser.NumberContext ctx) {
+    public Void visitNumber(lab1Parser.NumberContext ctx) {
         if (ctx.DecimalConst() != null) {
             exp += ctx.DecimalConst().getText();
         } else if (ctx.OctalConst() != null) {
