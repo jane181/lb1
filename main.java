8c0e6bd198b1bac582055ff22113c42cb8f72c05
jane181 <39273502+jane181@users.noreply.github.com>
2022-01-12 22:51:23 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,60 +1,98 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.ParseTree;
-
-import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 
-public class main {
-        public static void main(String[] args) throws IOException {
-            ErrorListener e = new ErrorListener();
-            FileInputStream file = new FileInputStream(args[0]);
-            String outputPath = "";
-            outputPath = args[1];
+public class Test {
+    public static String outputPath = "";
 
-            byte[] arr = new byte[100];
-            String str = "";
-            int i = 0;
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(args[0]);
+        outputPath = args[1];
 
-            while ((i = file.read(arr, 0, 100)) != -1) {
-                str += new String(arr, 0, i);
+        byte[] arr = new byte[100];
+        String s = "";
+        int i = 0;
+
+        while ((i = fis.read(arr, 0, 100)) != -1) {
+            s += new String(arr, 0, i);
+        }
+
+        String str = "";
+        boolean flag = false;
+        boolean flag2 = false;
+        while (true) {
+            if (s.charAt(0) == '/' && s.charAt(1) == '/' && !flag2) {
+                flag = true;
+                if (s.length() == 2) {
+                    break;
+                }
+                s = s.substring(2);
+                continue;
             }
-       PrintStream ps = null;
+            if (s.charAt(0) == '/' && s.charAt(1) == '*' && !flag) {
+                flag2 = true;
+                if (s.length() == 2) {
+                    break;
+                }
+                s = s.substring(2);
+                continue;
+            }
+            if (s.charAt(0) == '\n' && flag) {
+                flag = false;
+                str += "\n";
+                if (s.length() == 1) {
+                    break;
+                }
+                s = s.substring(1);
+                continue;
+            }
+            if (s.charAt(0) == '*' && s.charAt(1) == '/' && flag2) {
+                flag2 = false;
+                if (s.length() == 2) {
+                    break;
+                }
+                s = s.substring(2);
+                continue;
+            }
+            if (!flag && !flag2) {
+                str += s.charAt(0);
+                if (s.length() == 1) {
+                    break;
+                }
+                s = s.substring(1);
+                continue;
+            }
+            if (s.length() == 1) {
+                break;
+            }
+            s = s.substring(1);
+        }
+        if (flag2) {
+            System.out.println("error");
+            System.exit(1);
+        }
 
-        ps = new PrintStream(new FileOutputStream(outputPath));
 
-        //对每一个输入的字符串，构造一个 ANTLRStringStream 流 in
-        CharStream in = CharStreams.fromString(str);
-        ;
-
-        //用 in 构造词法分析器 lexer，词法分析的作用是产生记号
-        lb1Lexer lexer = new lb1Lexer(in);
-
-        //final Listener listener = new Listener(ref);
+        CharStream inputStream = CharStreams.fromString(str); // 获取输入流
+        lab2Lexer lexer = new lab2Lexer(inputStream);
         lexer.removeErrorListeners();
+        lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
 
-        lexer.addErrorListener(e);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer); // 词法分析获取 token 流
+        lab2Parser parser = new lab2Parser(tokenStream);
+        parser.removeErrorListeners();
+        parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 
-        //用词法分析器 lexer 构造一个记号流 tokens
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        //再使用 tokens 构造语法分析器 parser,至此已经完成词法分析和语法分析的准备工作
-        lb1Parser parser = new lb1Parser(tokens);
-       parser.removeErrorListeners();
-
-        parser.addErrorListener(e);
-
-        //最终调用语法分析器的规则 prog，完成对表达式的验证
-        //parser.prog();
         ParseTree tree = parser.compUnit(); // 获取语法树的根节点
+        // System.out.println(tree.toStringTree(parser)); // 打印字符串形式的语法树
 
-        visitor visitor = new visitor();
+        Visitor visitor = new Visitor();
         visitor.visit(tree);
-         file.close();
+
+        fis.close();
     }
 }
