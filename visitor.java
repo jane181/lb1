e5a4a9b5319651c21df76fed9e6c51759ab1e060
jane181 <39273502+jane181@users.noreply.github.com>
2022-01-13 23:32:38 +0800

    Update visitor.java

--- visitor.java
+++ visitor.java
@@ -3,12 +3,7 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 public class visitor extends lb1BaseVisitor<Void> {
-   public PrintStream ps = new PrintStream(new FileOutputStream(main.outputPath));
-    public static String exp = "";
-
-    public visitor() throws FileNotFoundException {
-        System.setOut(ps);
-    }
+  
 
    @Override
     public Void visitCompUnit(lb1Parser.CompUnitContext ctx) {
