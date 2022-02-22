319d9f870e7a776118c123d48293e41c91a1f331
jane181 <39273502+jane181@users.noreply.github.com>
2022-02-22 16:37:30 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,63 +1,54 @@
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.*;
 
 public class main {
-public static String outputPath="";
-public static String inputPath="";
-    public static void main(String[] args) throws IOException {
-        outputPath = args[1];
-        inputPath = args[0];
+    public static void main(String[] args) {
         ErrorListener e = new ErrorListener();
+        String input_path = args[0];
+        String output_path = args[1];
 
-        FileOutputStream file = new FileOutputStream(outputPath);
+        // 输出重定向到目标文件
+        File file = new File(output_path);
+        PrintStream stream = null;
+        try {
+            stream = new PrintStream(file);
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
+        System.setOut(stream);
 
-//       PrintStream f = null;
-//        try {
-//            f= new PrintStream(file);
-//        } catch (FileNotFoundException fileNotFoundException) {
-//            fileNotFoundException.printStackTrace();
-//        }
-       System.setOut(new PrintStream(file));
-
-
-        String str = useBufread(inputPath);
-        CharStream inputStream = CharStreams.fromString(str); // 获取输入流
-        lb1Lexer lexer = new lb1Lexer(inputStream);
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(e);
-
+        String input = fileToString(input_path); // get the input
+        CharStream inputStream = CharStreams.fromString(input); // 获取输入流
+       lb1Lexer lexer = new lb1Lexer(inputStream);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer); // 词法分析获取 token 流
-        lb1Parser parser = new lb1Parser(tokenStream);
+         lb1Parser parser = new lb1Parser(tokenStream);
         parser.removeErrorListeners();
         parser.addErrorListener(e);
-
         ParseTree tree = parser.compUnit(); // 获取语法树的根节点
-
         visitor visitor = new visitor();
         visitor.visit(tree);
-
-        file.close();
     }
-    public static String useBufread(String fileName) {
+
+    public static String fileToString(String fileName) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             StringBuilder stringBuilder = new StringBuilder();
-            String line = null;
-            String ls = System.getProperty("line.separator");
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
-                stringBuilder.append(ls);
+            char[] buffer = new char[10];
+            while (reader.read(buffer) != -1) {
+                stringBuilder.append(new String(buffer));
+                buffer = new char[1];
             }
-            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
             reader.close();
             return stringBuilder.toString();
         }
-        catch (IOException e) {
-            e.printStackTrace();
+        catch (Exception e) {
+            System.out.println(e);
         }
         return "";
     }
+}
