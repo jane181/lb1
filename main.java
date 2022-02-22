d4f80c44d6ce77c6b9a65cbcfaa8130467c4ffa9
jane181 <39273502+jane181@users.noreply.github.com>
2022-02-22 16:32:02 +0800

    Update main.java

--- main.java
+++ main.java
@@ -3,28 +3,28 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 
 public class main {
-
+public static String outputPath="";
+public static String inputPath="";
     public static void main(String[] args) throws IOException {
-            ErrorListener e = new ErrorListener();
-            FileInputStream file = new FileInputStream(args[0]);
-            String outputPath = "";
-            outputPath = args[1];
+        outputPath = args[1];
+        inputPath = args[0];
+        ErrorListener e = new ErrorListener();
 
-            byte[] arr = new byte[100];
-            String str = "";
-            int i = 0;
+        FileOutputStream file = new FileOutputStream(outputPath);
 
-            while ((i = file.read(arr, 0, 100)) != -1) {
-                str += new String(arr, 0, i);
-            }
-       
-    
+//       PrintStream f = null;
+//        try {
+//            f= new PrintStream(file);
+//        } catch (FileNotFoundException fileNotFoundException) {
+//            fileNotFoundException.printStackTrace();
+//        }
+       System.setOut(new PrintStream(file));
+
+
+        String str = useBufread(inputPath);
         CharStream inputStream = CharStreams.fromString(str); // 获取输入流
         lb1Lexer lexer = new lb1Lexer(inputStream);
         lexer.removeErrorListeners();
@@ -36,10 +36,28 @@ public class main {
         parser.addErrorListener(e);
 
         ParseTree tree = parser.compUnit(); // 获取语法树的根节点
-    
+
         visitor visitor = new visitor();
         visitor.visit(tree);
 
         file.close();
     }
-}
+    public static String useBufread(String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            reader.close();
+            return stringBuilder.toString();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
