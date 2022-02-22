a5079ae3f3cc50fbac40b2be0064725bba747e9a
jane181 <39273502+jane181@users.noreply.github.com>
2022-02-22 18:10:50 +0800

    Update visitor.java

--- visitor.java
+++ visitor.java
@@ -1,11 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class visitor extends lb1BaseVisitor<Void> {
-    @Override
-    public Void visitBlock(lb1Parser.BlockContext ctx) {
-        System.out.println("{");
-        visit(ctx.stmt());
-        System.out.println("\n}");
-        return null;
-    }
+
 
     @Override
     public Void visitCompUnit(lb1Parser.CompUnitContext ctx) {
@@ -14,13 +12,42 @@ public class visitor extends lb1BaseVisitor<Void> {
 
     @Override
     public Void visitFuncDef(lb1Parser.FuncDefContext ctx) {
-        System.out.printf("define dso_local i32 @main()");
+        System.out.print("define dso_local ");
         return super.visitFuncDef(ctx);
     }
 
     @Override
+    public Void visitFuncType(lb1Parser.FuncTypeContext ctx) {
+        System.out.print("i32 ");
+        return null;
+    }
+
+    @Override
+    public Void visitIdent(lb1Parser.IdentContext ctx) {
+        System.out.print("@main() ");
+        return null;
+    }
+
+    @Override
+    public Void visitBlock(lb1Parser.BlockContext ctx) {
+        System.out.println("{");
+        visit(ctx.stmt());
+        System.out.println("}");
+        return null;
+    }
+
+    @Override
+    public Void visitStmt(lb1Parser.StmtContext ctx) {
+        System.out.print("  ret ");
+        // visit(ctx.number());
+//       new PostfixExpression().func(exp);
+        //return null;
+        return super.visitStmt(ctx);
+    }
+
+    @Override
     public Void visitNumber(lb1Parser.NumberContext ctx) {
-       int number = 0;
+        int number = 0;
         if (ctx.decimalconst() != null) {
             number = Integer.parseInt(ctx.decimalconst().toString());
         } else if (ctx.octalconst() != null) {
@@ -30,13 +57,47 @@ public class visitor extends lb1BaseVisitor<Void> {
             String hex = ctx.hexadecimalconst().toString().substring(2);
             number =Integer.valueOf(hex, 16);
         }
-        System.out.print("i32 " + number);
+        System.out.print(number);
         return super.visitNumber(ctx);
     }
 
     @Override
-    public Void visitStmt(lb1Parser.StmtContext ctx) {
-        System.out.print("\tret ");
-        return super.visitStmt(ctx);
+    public Void visitDecimalconst(lb1Parser.DecimalconstContext ctx) {
+        return super.visitDecimalconst(ctx);
+    }
+
+    @Override
+    public Void visitOctalconst(lb1Parser.OctalconstContext ctx) {
+        return super.visitOctalconst(ctx);
+    }
+
+    @Override
+    public Void visitHexadecimalconst(lb1Parser.HexadecimalconstContext ctx) {
+        return super.visitHexadecimalconst(ctx);
+    }
+
+    @Override
+    public Void visitHexadecimalprefix(lb1Parser.HexadecimalprefixContext ctx) {
+        return super.visitHexadecimalprefix(ctx);
+    }
+
+    @Override
+    public Void visitNonzerodigit(lb1Parser.NonzerodigitContext ctx) {
+        return super.visitNonzerodigit(ctx);
+    }
+
+    @Override
+    public Void visitOctaldigit(lb1Parser.OctaldigitContext ctx) {
+        return super.visitOctaldigit(ctx);
+    }
+
+    @Override
+    public Void visitDigit(lb1Parser.DigitContext ctx) {
+        return super.visitDigit(ctx);
+    }
+
+    @Override
+    public Void visitHexadecimaldigit(lb1Parser.HexadecimaldigitContext ctx) {
+        return super.visitHexadecimaldigit(ctx);
     }
 }
