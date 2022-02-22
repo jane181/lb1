00fba812130db801d38d43dabd9357a858c80371
jane181 <39273502+jane181@users.noreply.github.com>
2022-02-22 17:26:54 +0800

    Update ErrorListener.java

--- ErrorListener.java
+++ ErrorListener.java
@@ -4,14 +4,35 @@ import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import java.util.BitSet;
-
-public class ErrorListener extends BaseErrorListener  {
+public class ErrorListener implements ANTLRErrorListener {
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
+        System.exit(-1);
+    }
 
     @Override
-    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
-            throws ParseCancellationException {
-        System.exit(1);
+    public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
+        System.exit(-1);
     }
+
+    @Override
+    public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
+        System.exit(-1);
+    }
+
+    @Override
+    public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
+        System.exit(-1);
+    }
+
+
+// public class ErrorListener extends BaseErrorListener  {
+
+//     @Override
+//     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+//             throws ParseCancellationException {
+//         System.exit(1);
+//     }
 
  //   @Override
 //     public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
