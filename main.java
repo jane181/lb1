ae236caa25d17e7dd26bf7168a7cd0f9667642b8
jane181 <39273502+jane181@users.noreply.github.com>
2022-02-22 16:51:46 +0800

    Update main.java

--- main.java
+++ main.java
@@ -7,22 +7,25 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import java.io.*;
 
 public class main {
+public static String outputPath="";
+public static String inputPath="";
     public static void main(String[] args) {
         ErrorListener e = new ErrorListener();
-        String input_path = args[0];
-        String output_path = args[1];
-
-        File file = new File(output_path);
+        inputPath = args[0];
+        outputPath = args[1];
         
-        PrintStream stream = null;
+        
+        File file = new File(outputPath);
+       
+        PrintStream ps = null;
         try {
-            stream = new PrintStream(file);
+            ps = new PrintStream(file);
         } catch (FileNotFoundException fileNotFoundException) {
             fileNotFoundException.printStackTrace();
         }
-        System.setOut(stream);
+        System.setOut(ps);
         
-        String input = fileToString(input_path); // get the input
+        String input = fileToString(inputPath); // get the input
         CharStream inputStream = CharStreams.fromString(input); // 获取输入流
        lb1Lexer lexer = new lb1Lexer(inputStream);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer); // 词法分析获取 token 流
